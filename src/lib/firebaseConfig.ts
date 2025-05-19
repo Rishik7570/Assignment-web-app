bc5e63e6d2f0e0b9b0454e9e3fc959789b490d74
Rishik7570 <rishiksingha@gmail.com>
2025-05-20 02:12:20 +0530

    1st

--- src/lib/firebaseConfig.ts
+++ src/lib/firebaseConfig.ts
@@ -6,12 +6,12 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET!,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
+  apiKey: "AIzaSyAtUYAbfib99tecrRopOYds163z8B0PX7Q",
+  authDomain: "login-cb902.firebaseapp.com",
+  projectId: "login-cb902",
+  storageBucket: "login-cb902.firebasestorage.app",
+  messagingSenderId: "394182967245",
+  appId: "1:394182967245:web:dc1d54b98b491fa2184e84"
 };
 
 // Initialize Firebase
